b0493810ed9c94bee71d2f8069f24e0e09121262
wisdomer690 <c.arljhon879@gmail.com>
2025-05-03 19:25:10 +0100

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -2,10 +2,10 @@
 module.exports = {
     enabled: true, 
     lavalink: {
-        identifier: 'GlaceYT',
-        password: 'glaceyt',
-        host: '193.226.78.187',
-        port: 9372,
-        secure: false
+        host: "lavalink.jirayu.net",
+        password: "youshallnotpass",
+        port: 13592,
+        secure: false,
+        name: "Main Node"
     }
 };
